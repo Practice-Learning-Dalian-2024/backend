3a389e727b0d31a7da1d7752022d477f0f7360ac
cclvi256 <51155451+cclvi256@users.noreply.github.com>
2024-07-26 14:44:15 +0800

    feat: Add 5 tables in database design

---
+++ src/main/database/reset.sql
@@ -0,0 +1,61 @@
+DROP TABLE IF EXISTS user_info;
+CREATE TABLE IF NOT EXISTS user_info
+(
+    id            SERIAL PRIMARY KEY,
+    cancelled     BOOLEAN            NOT NULL DEFAULT FALSE,
+    username      VARCHAR(40) UNIQUE NOT NULL,
+    name          VARCHAR(70)        NOT NULL,
+    role          BOOLEAN            NOT NULL DEFAULT FALSE,
+    gender        VARCHAR(255)       NOT NULL,
+    salt          CHAR(16)           NOT NULL,
+    password_hash CHAR(64)           NOT NULL,
+    register_time TIMESTAMP          NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    avatar_path   VARCHAR(255),
+    email         VARCHAR(100),
+    phone         VARCHAR(20),
+    site          VARCHAR(255),
+    bio           TEXT
+);
+
+DROP TABLE IF EXISTS common_user;
+CREATE TABLE IF NOT EXISTS common_user
+(
+    id      SERIAL PRIMARY KEY,
+    user_id INT REFERENCES user_info (id) ON DELETE CASCADE NOT NULL
+);
+
+DROP TABLE IF EXISTS admin;
+CREATE TABLE IF NOT EXISTS admin
+(
+    id      SERIAL PRIMARY KEY,
+    user_id INT REFERENCES user_info (id) ON DELETE CASCADE NOT NULL
+);
+
+DROP TABLE IF EXISTS blog;
+CREATE TABLE IF NOT EXISTS blog
+(
+    id            SERIAL PRIMARY KEY,
+    author        INT REFERENCES user_info (id) NOT NULL,
+    title         VARCHAR(255) NOT NULL,
+    content_path  VARCHAR(255) NOT NULL,
+    time          TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    category      SMALLINT     NOT NULL,
+    visitor_count INT          NOT NULL DEFAULT 0,
+    like_count    INT          NOT NULL DEFAULT 0,
+    status        SMALLINT     NOT NULL DEFAULT 0,
+    brief         VARCHAR(255) NOT NULL
+);
+
+DROP TABLE IF EXISTS comment;
+CREATE TABLE IF NOT EXISTS comment
+(
+    id          SERIAL PRIMARY KEY,
+    commenter   INT REFERENCES user_info (id) NOT NULL,
+    blog        INT REFERENCES blog (id) ON DELETE CASCADE NOT NULL,
+    is_nested   BOOLEAN NOT NULL,
+    parent      INT REFERENCES comment (id),
+    content     TEXT NOT NULL,
+    time        TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    like_count  INT NOT NULL DEFAULT 0,
+    status      SMALLINT NOT NULL DEFAULT 0
+);
