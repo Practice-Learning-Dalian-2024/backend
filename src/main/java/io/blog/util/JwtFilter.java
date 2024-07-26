c9969a4245c5a57a1455d77354562598ed92bf6e
cclvi256 <51155451+cclvi256@users.noreply.github.com>
2024-07-26 17:59:29 +0800

    ci: Add JWT utils for token authentication

---
+++ src/main/java/io/blog/util/JwtFilter.java
@@ -0,0 +1,26 @@
+package io.blog.util;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+        String token = request.getHeader("Authorization");
+
+        if (token != null && JwtUtil.validateToken(token)) {
+            filterChain.doFilter(request, response);
+        } else {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        }
+    }
+}
