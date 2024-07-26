90f4aa05273151fdd2f4f891e04efac19d7e5154
cclvi256 <51155451+cclvi256@users.noreply.github.com>
2024-07-26 17:57:39 +0800

    feat: Modified the database

--- database/reset.sql
+++ database/reset.sql
@@ -4,9 +4,9 @@ CREATE TABLE IF NOT EXISTS user_info
     id            SERIAL PRIMARY KEY,
     cancelled     BOOLEAN            NOT NULL DEFAULT FALSE,
     username      VARCHAR(40) UNIQUE NOT NULL,
-    name          VARCHAR(70)        NOT NULL,
+    name          VARCHAR(70),
     role          BOOLEAN            NOT NULL DEFAULT FALSE,
-    gender        VARCHAR(255)       NOT NULL,
+    gender        VARCHAR(255)       NOT NULL DEFAULT 'uncertain',
     salt          CHAR(16)           NOT NULL,
     password_hash CHAR(64)           NOT NULL,
     register_time TIMESTAMP          NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -36,26 +36,26 @@ CREATE TABLE IF NOT EXISTS blog
 (
     id            SERIAL PRIMARY KEY,
     author        INT REFERENCES user_info (id) NOT NULL,
-    title         VARCHAR(255) NOT NULL,
-    content_path  VARCHAR(255) NOT NULL,
-    time          TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    category      SMALLINT     NOT NULL,
-    visitor_count INT          NOT NULL DEFAULT 0,
-    like_count    INT          NOT NULL DEFAULT 0,
-    status        SMALLINT     NOT NULL DEFAULT 0,
-    brief         VARCHAR(255) NOT NULL
+    title         VARCHAR(255)                  NOT NULL,
+    content_path  VARCHAR(255)                  NOT NULL,
+    time          TIMESTAMP                     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    category      SMALLINT                      NOT NULL,
+    visitor_count INT                           NOT NULL DEFAULT 0,
+    like_count    INT                           NOT NULL DEFAULT 0,
+    status        SMALLINT                      NOT NULL DEFAULT 0,
+    brief         VARCHAR(255)                  NOT NULL
 );
 
 DROP TABLE IF EXISTS comment;
 CREATE TABLE IF NOT EXISTS comment
 (
-    id          SERIAL PRIMARY KEY,
-    commenter   INT REFERENCES user_info (id) NOT NULL,
-    blog        INT REFERENCES blog (id) ON DELETE CASCADE NOT NULL,
-    is_nested   BOOLEAN NOT NULL,
-    parent      INT REFERENCES comment (id),
-    content     TEXT NOT NULL,
-    time        TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    like_count  INT NOT NULL DEFAULT 0,
-    status      SMALLINT NOT NULL DEFAULT 0
+    id         SERIAL PRIMARY KEY,
+    commenter  INT REFERENCES user_info (id)              NOT NULL,
+    blog       INT REFERENCES blog (id) ON DELETE CASCADE NOT NULL,
+    is_nested  BOOLEAN                                    NOT NULL,
+    parent     INT REFERENCES comment (id) ON DELETE CASCADE,
+    content    TEXT                                       NOT NULL,
+    time       TIMESTAMP                                  NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    like_count INT                                        NOT NULL DEFAULT 0,
+    status     SMALLINT                                   NOT NULL DEFAULT 0
 );
