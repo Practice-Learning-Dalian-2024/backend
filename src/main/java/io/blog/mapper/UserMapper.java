a29e87198bf67060a9113fe63d95a99d1f673db4
cclvi256 <51155451+cclvi256@users.noreply.github.com>
2024-07-27 10:47:17 +0800

    feat: Add the mapper of user info

---
+++ src/main/java/io/blog/mapper/UserMapper.java
@@ -0,0 +1,21 @@
+package io.blog.mapper;
+
+import io.blog.model.database.UserInfo;
+import io.blog.model.response.UserCommonInfo;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Results;
+
+import java.util.List;
+
+@Mapper
+public interface UserMapper {
+    // Can CREATE, READ, UPDATE, and DELETE user.
+    void create(UserInfo user);
+    void update(UserInfo user);
+    void delete(int id);
+    UserCommonInfo read(int id);
+    List<UserCommonInfo> readAll();
+
+    void updatePassword(int id, String salt, String passwordHash);
+}
