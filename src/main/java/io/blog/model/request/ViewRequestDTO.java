72eefc09b8980782441c3ee2047d393fbde084f6
cclvi256 <51155451+cclvi256@users.noreply.github.com>
2024-07-29 17:54:34 +0800

    fix: Complete model annotations

--- src/main/java/io/blog/model/request/ViewRequestDTO.java
+++ src/main/java/io/blog/model/request/ViewRequestDTO.java
@@ -1,5 +1,12 @@
 package io.blog.model.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ViewRequestDTO {
     private int blogId;
     private int userId;
