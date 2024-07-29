96c59d4bf184d9e9bb897969e2d189e7b08b4baf
cclvi256 <51155451+cclvi256@users.noreply.github.com>
2024-07-29 14:51:18 +0800

    ci: Completed all the schemas in the Java mapper

--- src/main/java/io/blog/mapper/BlogMapper.java
+++ src/main/java/io/blog/mapper/BlogMapper.java
@@ -1,8 +1,16 @@
 package io.blog.mapper;
 
+import io.blog.model.request.BlogRequestDTO;
+import io.blog.model.response.BlogResponseDTO;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface BlogMapper {
-
+    void create(BlogRequestDTO blog);
+    void update(BlogRequestDTO blog);
+    void delete(int id);
+    BlogResponseDTO read(int id);
+    List<BlogResponseDTO> readAll();
 }
