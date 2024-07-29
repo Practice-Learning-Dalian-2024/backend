967e0948ba88b562ba0e2abf81ce995dbf779571
cclvi256 <51155451+cclvi256@users.noreply.github.com>
2024-07-29 14:12:27 +0800

    ci: Add DTO for frontend request of blog

---
+++ src/main/java/io/blog/model/request/BlogRequestDTO.java
@@ -0,0 +1,18 @@
+package io.blog.model.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BlogRequestDTO {
+//    INSERT INTO blog (author, title, content_path, category, brief)
+//    VALUES (#{author}, #{title}, #{contentPath}, #{category}, #{brief})
+    private int author;
+    private String title;
+    private String contentPath;
+    private short category;
+    private String brief;
+}
