1bc7d4803fdc34f8c445cac134729142fdc13196
cclvi256 <51155451+cclvi256@users.noreply.github.com>
2024-07-29 11:07:55 +0800

    ci: Add the controller implementation list

---
+++ src/main/java/io/blog/controller/BlogController.java
@@ -0,0 +1,46 @@
+package io.blog.controller;
+
+import io.blog.util.Response;
+import jakarta.websocket.server.PathParam;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/blog")
+public class BlogController {
+    // TODO
+    @GetMapping("/{id}")
+    public Response<?> getBlog(@PathVariable int id) {
+        throw new RuntimeException("TODO"); // TODO
+    }
+
+    @GetMapping("/dashboard/{id}")
+    public Response<?> getDashboard(@PathVariable int id) {
+        throw new RuntimeException("TODO"); // TODO
+    }
+
+    @PostMapping("/new")
+    public Response<?> newBlog(@RequestBody String content) {
+        throw new RuntimeException("TODO"); // TODO
+    }
+
+    @PutMapping("/edit")
+    public Response<?> editBlog(@RequestBody String content) {
+        throw new RuntimeException("TODO"); // TODO
+    }
+
+    @DeleteMapping("/del")
+    public Response<?> deleteBlog(@RequestBody String content) {
+        throw new RuntimeException("TODO"); // TODO
+    }
+
+    @PostMapping("/like")
+    public Response<?> likeBlog(@RequestBody String content) {
+        throw new RuntimeException("TODO"); // TODO
+    }
+
+    @PostMapping("/comment")
+    public Response<?> commentBlog(@RequestBody String content) {
+        throw new RuntimeException("TODO"); // TODO
+    }
+}
