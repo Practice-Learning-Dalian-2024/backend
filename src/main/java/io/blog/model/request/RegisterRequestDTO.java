92f6fd3b9b003fd3b52575a8c3a74af89d5178cd
cclvi256 <51155451+cclvi256@users.noreply.github.com>
2024-07-30 10:46:13 +0800

    fix: Add DTO for registering

---
+++ src/main/java/io/blog/model/request/RegisterRequestDTO.java
@@ -0,0 +1,13 @@
+package io.blog.model.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequestDTO {
+    private String username;
+    private String password;
+}
